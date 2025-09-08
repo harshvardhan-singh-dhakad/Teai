9b534f48d6335ecaa5c63ca27de8d0e7c60d7123
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-09-08 18:08:03 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -17,7 +17,7 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
     }
   }, [initialValue, key]);
 
-  const [storedValue, setStoredValue] = useState<T>(readValue);
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
 
   const setValue = (value: T | ((val: T) => T)) => {
     try {
@@ -33,7 +33,8 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
   
   useEffect(() => {
     setStoredValue(readValue());
-  }, [readValue]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return [storedValue, setValue];
 }
